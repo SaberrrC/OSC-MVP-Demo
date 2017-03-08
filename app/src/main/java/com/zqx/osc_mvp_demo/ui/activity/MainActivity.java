ab4e58363396aad55456b28532466b6157c8c162
zqx <zqx@zqx.com>
2017-03-08 20:05:14 +0800

    添加子条目布局

--- app/src/main/java/com/zqx/osc_mvp_demo/ui/activity/MainActivity.java
+++ app/src/main/java/com/zqx/osc_mvp_demo/ui/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.zqx.osc_mvp_demo.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 
 import com.zqx.osc_mvp_demo.R;
+import com.zqx.osc_mvp_demo.ui.widget.SimpleRecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler)
+    SimpleRecyclerView mRecycler;
+    @BindView(R.id.swipe)
+    SwipeRefreshLayout mSwipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mRecycler.itemLayout(R.layout.item_ask_answer);
     }
 }
